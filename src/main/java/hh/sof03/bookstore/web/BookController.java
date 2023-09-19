55c61d47956b8cad41d4ee5983d96e756aede977
elisamarjut <117983133+elisamarjut@users.noreply.github.com>
2023-09-19 18:18:09 +0300

    tehtävän C3.2 mukaiset muutokset

--- src/main/java/hh/sof03/bookstore/web/BookController.java
+++ src/main/java/hh/sof03/bookstore/web/BookController.java
@@ -1,13 +1,22 @@
 package hh.sof03.bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import hh.sof03.bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
 
-    @GetMapping("/index")
-    public String listBooks() {
+    @Autowired
+    private BookRepository repository;
+
+    @GetMapping("/booklist")
+    public String listBooks(Model model) {
+        model.addAttribute("books", repository.findAll());
         return "booklist";
     }
+
 }
