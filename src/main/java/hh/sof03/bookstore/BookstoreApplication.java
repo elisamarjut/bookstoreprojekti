32ec617b65d5a8e94d3e213227ae35e036cdce96
elisamarjut <117983133+elisamarjut@users.noreply.github.com>
2023-09-19 17:36:25 +0300

    tehtävän C3.1 mukaiset muokkaukset

--- src/main/java/hh/sof03/bookstore/BookstoreApplication.java
+++ src/main/java/hh/sof03/bookstore/BookstoreApplication.java
@@ -1,13 +1,39 @@
 package hh.sof03.bookstore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import hh.sof03.bookstore.domain.Book;
+import hh.sof03.bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demodata(BookRepository repository) {
+		return (args) -> {
+			log.info("save a few demo books");
+			Book b1 = new Book("Rautakolmio", "Leena Lehtolainen", "9789520429348", 2021, 7.80);
+			Book b2 = new Book("Ensimmäinen murhani", "Leena Lehtolainen", "9789520422301", 2020, 15.50);
+			Book b3 = new Book("Harmin paikka", "Leena Lehtolainen", "9789520422318", 2020, 7.90);
+
+			repository.save(b1);
+			repository.save(b2);
+			repository.save(b3);
+
+			log.info("fetch all books");
+
+		};
+		// title, author, isbn, year, price
 	}
 
 }
--- src/main/java/hh/sof03/bookstore/domain/Book.java
+++ src/main/java/hh/sof03/bookstore/domain/Book.java
@@ -1,27 +1,40 @@
 package hh.sof03.bookstore.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Book {
 
-    private String title;
-    private String author;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String title, author, isbn;
+
+    @Column(name = "publishing_year")
     private int year;
-    private String isbn;
     private double price;
 
     public Book() {
-        this.title = null;
-        this.author = null;
-        this.year = 0;
-        this.isbn = null;
-        this.price = 0;
     }
 
-    public Book(String title, String author, int year, String isbn, double price) {
+    public Book(String title, String author, String isbn, int year, double price) {
         this.title = title;
         this.author = author;
-        this.year = year;
         this.isbn = isbn;
+        this.year = year;
         this.price = price;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     public String getTitle() {
@@ -40,20 +53,20 @@ public class Book {
         this.author = author;
     }
 
-    public int getYear() {
-        return year;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
     public String getIsbn() {
         return isbn;
     }
 
     public void setIsbn(String isbn) {
         this.isbn = isbn;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
     }
 
     public double getPrice() {
@@ -66,8 +79,8 @@ public class Book {
 
     @Override
     public String toString() {
-        return "Book [title=" + title + ", author=" + author + ", year=" + year + ", isbn=" + isbn + ", price=" + price
-                + "]";
+        return "Book [id=" + id + ", title=" + title + ", author=" + author + ", isbn=" + isbn + ", year=" + year
+                + ", price=" + price + "]";
     }
 
 }
