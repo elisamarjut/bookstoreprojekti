d94f2d9744a425821a000a277bfdbee19a1d47a2
elisamarjut <117983133+elisamarjut@users.noreply.github.com>
2023-09-19 19:04:08 +0300

    tehtävän C3.3 mukaiset lisäykset (add ja delete)

--- src/main/java/hh/sof03/bookstore/web/BookController.java
+++ src/main/java/hh/sof03/bookstore/web/BookController.java
@@ -3,9 +3,13 @@ package hh.sof03.bookstore.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import hh.sof03.bookstore.domain.Book;
 import hh.sof03.bookstore.domain.BookRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class BookController {
@@ -13,10 +17,31 @@ public class BookController {
     @Autowired
     private BookRepository repository;
 
-    @GetMapping("/booklist")
+    @RequestMapping("/booklist")
     public String listBooks(Model model) {
         model.addAttribute("books", repository.findAll());
         return "booklist";
+    }
+
+    // Add new book
+    @RequestMapping("/add")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    // Save new book
+    @PostMapping("/save")
+    public String save(Book book) {
+        repository.save(book);
+        return "redirect:booklist";
+    }
+
+    // Delete a book
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+        repository.deleteById(bookId);
+        return "redirect:../booklist";
     }
 
 }
