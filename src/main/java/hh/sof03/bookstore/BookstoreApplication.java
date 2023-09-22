69545b4be77040c9e99a63582557d20e9fb4aad1
elisamarjut <117983133+elisamarjut@users.noreply.github.com>
2023-09-22 22:00:44 +0300

    Lisätty Category-entity, CategoryRepository ja muutama demokategoria (tehtävä C4.1)

--- src/main/java/hh/sof03/bookstore/BookstoreApplication.java
+++ src/main/java/hh/sof03/bookstore/BookstoreApplication.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Bean;
 
 import hh.sof03.bookstore.domain.Book;
 import hh.sof03.bookstore.domain.BookRepository;
+import hh.sof03.bookstore.domain.Category;
+import hh.sof03.bookstore.domain.CategoryRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -19,21 +21,36 @@ public class BookstoreApplication {
 	}
 
 	@Bean
-	public CommandLineRunner demodata(BookRepository bookRepository) {
+	public CommandLineRunner demodata(BookRepository bookRepository, CategoryRepository categoryRepository) {
 		return (args) -> {
-			log.info("save a few demo books");
-			Book b1 = new Book("Rautakolmio", "Leena Lehtolainen", "9789520429348", 2021, 7.80);
-			Book b2 = new Book("Ensimmäinen murhani", "Leena Lehtolainen", "9789520422301", 2020, 15.50);
-			Book b3 = new Book("Harmin paikka", "Leena Lehtolainen", "9789520422318", 2020, 7.90);
+			log.info("Save a few categories");
+			Category c1 = new Category("Crime");
+			Category c2 = new Category("Romance");
+			Category c3 = new Category("Scifi");
+
+			categoryRepository.save(c1);
+			categoryRepository.save(c2);
+			categoryRepository.save(c3);
+
+			log.info("Save a few books");
+			Book b1 = new Book("Ensimmäinen murhani", "Leena Lehtolainen", "9789520422301", 2020, 7.80);
+			Book b2 = new Book("The Fault in Our Stars", "John Green", "9780141345659", 2013, 10.80);
+			Book b3 = new Book("Teemestarin kirja", "Emmi Itäranta", "9789518519747", 2019, 20.80);
 
 			bookRepository.save(b1);
 			bookRepository.save(b2);
 			bookRepository.save(b3);
 
-			log.info("fetch all books");
+			log.info("Fetch all categories");
+			for (Category category : categoryRepository.findAll()) {
+				log.info(category.toString());
+			}
+
+			log.info("Fetch all books");
 			for (Book book : bookRepository.findAll()) {
 				log.info(book.toString());
 			}
+
 		};
 	}
 
